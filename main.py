2cfbfeb1074c38f6a29a00ef2de37ba1129d19dc
Bryan Collazo <bcollazo2010@gmail.com>
2018-03-12 01:06:51 -0500

    Make main accept 2 player selection.  Human must be selected

--- main.py
+++ main.py
@@ -2,26 +2,30 @@
 from models.core import Game
 from models.players import HumanPlayer, RandomPlayer, LocalPlayer, SmartPlayer, KNNPlayer
 
-player = HumanPlayer('P1')
-# Choose computer player.
-print('Choose opponent:')
-print('\t1. Random Player')
-print('\t2. Local Player')
-print('\t3. Smart Player')
-print('\t4. KNN Player')
-i = raw_input('>>> ')
-while i not in ['1', '2', '3', '4', 'exit']:
-    print('Please enter a number from 1 to 3')
+
+def choose_player(name):
+    print('Choose %s:' % (name))
+    print('\t0. Human Player')
+    print('\t1. Random Player')
+    print('\t2. Local Player')
+    print('\t3. Smart Player')
+    print('\t4. KNN Player')
     i = raw_input('>>> ')
-if i == 'exit':
-    exit()
-computer = {
-    '1': lambda: RandomPlayer('P2'),
-    '2': lambda: LocalPlayer('P2'),
-    '3': lambda: SmartPlayer('P2'),
-    '4': lambda: KNNPlayer('P2', 10),
-}[i]()
+    while i not in ['0', '1', '2', '3', '4', 'exit']:
+        print('Please enter a number from 0 to 4')
+        i = raw_input('>>> ')
+    if i == 'exit':
+        exit()
+    return {
+        '0': lambda: HumanPlayer(name),
+        '1': lambda: RandomPlayer(name),
+        '2': lambda: LocalPlayer(name),
+        '3': lambda: SmartPlayer(name),
+        '4': lambda: KNNPlayer(name, 10),
+    }[i]()
+p1 = choose_player('P1')
+p2 = choose_player('P2')
 
 # Start game.
-game = Game(player, computer, verbose=True)
+game = Game(p1, p2, verbose=True)
 game.play()
