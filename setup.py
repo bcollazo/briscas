98e463a7dc71c030567ae3c22604ae7df9e07ca8
Bryan Collazo <bcollazo2010@gmail.com>
2018-03-12 22:52:05 -0500

    Setup changes

--- setup.py
+++ setup.py
@@ -7,8 +7,12 @@ setup(
     description='Library to model the briscas card game.',
     author='Bryan Collazo',
     author_email='bcollazo2010@gmail.com',
+    license='MIT',
     url='https://github.com/bcollazo/briscas',
     download_url='https://github.com/bcollazo/briscas/archive/0.1.tar.gz',
     keywords=['briscas', 'card', 'game', 'terminal', 'naipes'],
-    classifiers=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+    ],
 )
