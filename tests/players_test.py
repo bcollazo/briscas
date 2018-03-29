8c2c407f3cff4e59cbd0da8f8024105404770c37
Bryan Collazo <bcollazo2010@gmail.com>
2018-03-29 19:16:34 -0400

    Add tests for LocalPlayer

--- tests/players_test.py
+++ tests/players_test.py
@@ -21,5 +21,14 @@ class PlayersTest(unittest.TestCase):
         p = HumanPlayer('Bryan', print_fn=print_mock)
         self._assert_play_pops_card(p)
 
+    def test_play_pops_card_with_thrown(self):
         p = LocalPlayer('Bryan')
-        self._assert_play_pops_card(p)
+        p.init([Card(4, Suite.ORO), Card(5, Suite.ORO), Card(6, Suite.ORO)])
+        card = p.play(Card(2, Suite.ORO), thrown=Card(3, Suite.ORO))  # has no better hand
+        self.assertEqual(len(p.hand), 2)
+        self.assertEqual(card.number, 4)
+
+        p.init([Card(4, Suite.ORO), Card(6, Suite.ORO), Card(7, Suite.ORO)])
+        card = p.play(Card(1, Suite.ORO), thrown=Card(5, Suite.ORO))  # has better hand
+        self.assertEqual(len(p.hand), 2)
+        self.assertEqual(card.number, 6)
