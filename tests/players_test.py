f1d0f5558e4a5d905485e81254f7652ef2fae69d
Bryan Collazo <bcollazo2010@gmail.com>
2018-03-29 19:23:15 -0400

    Fix flake8

--- tests/players_test.py
+++ tests/players_test.py
@@ -23,12 +23,15 @@ class PlayersTest(unittest.TestCase):
 
     def test_play_pops_card_with_thrown(self):
         p = LocalPlayer('Bryan')
+
+        # has no better hand
         p.init([Card(4, Suite.ORO), Card(5, Suite.ORO), Card(6, Suite.ORO)])
-        card = p.play(Card(2, Suite.ORO), thrown=Card(3, Suite.ORO))  # has no better hand
+        card = p.play(Card(2, Suite.ORO), thrown=Card(3, Suite.ORO))
         self.assertEqual(len(p.hand), 2)
         self.assertEqual(card.number, 4)
 
+        # has better hand
         p.init([Card(4, Suite.ORO), Card(6, Suite.ORO), Card(7, Suite.ORO)])
-        card = p.play(Card(1, Suite.ORO), thrown=Card(5, Suite.ORO))  # has better hand
+        card = p.play(Card(1, Suite.ORO), thrown=Card(5, Suite.ORO))
         self.assertEqual(len(p.hand), 2)
         self.assertEqual(card.number, 6)
