68727796573350a83744c1b45d884ff451806096
Bryan Collazo <bcollazo2010@gmail.com>
2018-04-22 21:52:23 -0400

    Tag to version 1.0

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='briscas',
     packages=['briscas'],
-    version='0.2',
+    version='1.0',
     description='Library to model the briscas card game.',
     author='Bryan Collazo',
     author_email='bcollazo2010@gmail.com',
     license='MIT',
     url='https://github.com/bcollazo/briscas',
-    download_url='https://github.com/bcollazo/briscas/archive/0.1.tar.gz',
+    download_url='https://github.com/bcollazo/briscas/archive/1.0.tar.gz',
     keywords=['briscas', 'card', 'game', 'terminal', 'naipes'],
     classifiers=[
         'Development Status :: 3 - Alpha',
