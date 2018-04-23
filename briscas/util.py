c33d04f415127ce5fd64d5da6779b5139a7ce814
Bryan Collazo <bcollazo2010@gmail.com>
2018-04-22 22:45:16 -0400

    Modernize input

--- briscas/util.py
+++ briscas/util.py
@@ -2,13 +2,13 @@ from __future__ import absolute_import
 
 from briscas.models import Suite, Colors
 
-from six.moves import range
+from six.moves import input, range
 
 
 def ask_for_input(prompt, allowed, input_fn=input, exit_fn=exit):
-    i = input_fn(prompt)
+    i = str(input_fn(prompt))
     while i not in allowed and i != 'exit':
-        i = input_fn(prompt)
+        i = str(input_fn(prompt))
     if i == 'exit':
         exit_fn()
     return i
